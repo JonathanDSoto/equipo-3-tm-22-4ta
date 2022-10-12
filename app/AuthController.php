da92a7073714fef95c7ace3fa7db462f77555003
Dracon-151 <demianalexisalvarado@gmail.com>
2022-10-11 23:40:44 -0600

    Logout en AuthController

--- app/AuthController.php
+++ app/AuthController.php
@@ -18,8 +18,18 @@ if (isset($_POST['action'])) {
 				$authController->login($email,$password);
 
 			break; 
+			case 'logout':
+
+				$authController = new AuthController();
+				$authController->logout();
+			
+			break; 
 		}
 
+	}
+	else{
+		session_destroy();
+		header("Location:".BASE_PATH."?error=true");
 	}
 }
 
@@ -58,25 +68,44 @@ Class AuthController{
 			$_SESSION['avatar']= $response->data->avatar;
 			$_SESSION['token']= $response->data->token;
 
-			header("Location:".BASE_PATH."products");
+			header("Location:".BASE_PATH."index");
 		}else{
-
-
 			#var_dump($response);
 			header("Location:".BASE_PATH."?error=true");
 		}
 
 	}
+	public function logout(){
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => 'http://crud.jonathansoto.mx/api/logout',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'POST',
+		CURLOPT_POSTFIELDS => array('email' => $_SESSION['email']),
+		CURLOPT_HTTPHEADER => array(
+			'Authorization: Bearer '.$_SESSION['token']
+		),
+		));
+
+		$response = curl_exec($curl); 
+		curl_close($curl);
+		$response = json_decode($response);
+
+		if ( isset($response->code) && $response->code > 0) {
+
+			session_destroy();
+			header("Location:".BASE_PATH.'?success=true');
+		}else{
+			#var_dump($response);
+			header("Location:".BASE_PATH."index?error=true");
+		}
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
 ?>
