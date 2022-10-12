3511180c94b6d41290434278d4bbd6e63a923dc5
Isaac Pérez <56978632+Isaac13Di00@users.noreply.github.com>
2022-10-11 23:35:41 -0600

    nuevas carpetas vista-productos/login

--- layouts/sidebar.template.php
+++ layouts/sidebar.template.php
@@ -1,8 +1,8 @@
-        <div class="app-menu navbar-menu">
+<div class="app-menu navbar-menu">
             <!-- LOGO -->
             <div class="navbar-brand-box">
                 <!-- Dark Logo-->
-                <a href="index.html" class="logo logo-dark">
+                <a href="<?= BASE_PATH ?>views/products/index.php" class="logo logo-dark">
                     <span class="logo-sm">
                         <img src="<?= BASE_PATH ?>public/images/logo-sm.png" alt="" height="22">
                     </span>
@@ -11,7 +11,7 @@
                     </span>
                 </a>
                 <!-- Light Logo-->
-                <a href="index.html" class="logo logo-light">
+                <a href="<?= BASE_PATH ?>views/products/index.php" class="logo logo-light">
                     <span class="logo-sm">
                         <img src="<?= BASE_PATH ?>public/images/logo-sm.png" alt="" height="22">
                     </span>
@@ -26,80 +26,55 @@
 
             <div id="scrollbar">
                 <div class="container-fluid">
-
                     <div id="two-column-menu">
                     </div>
                     <ul class="navbar-nav" id="navbar-nav">
                         <li class="menu-title"><span data-key="t-menu">Menu</span></li>
-                        
-
                         <li class="nav-item">
                             <a class="nav-link menu-link" href="widgets.html">
-                                <i class="mdi mdi-puzzle-outline"></i> <span data-key="t-widgets">Usuarios</span>
+                                <i class="ri-user-2-line"></i> <span data-key="t-widgets">Users</span>
                             </a>
                         </li>
-
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="widgets.html">
+                                <i class="ri-user-3-line"></i> <span data-key="t-widgets">Clients</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="widgets.html">
+                                <i class="ri-dashboard-line"></i> <span data-key="t-dashboards">Products</span>
+                            </a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
-                                <i class="mdi mdi-speedometer"></i> <span data-key="t-dashboards">Dashboards</span>
+                                <i class="ri-clipboard-line"></i> <span data-key="t-dashboards">Catalogue</span>
                             </a>
                             <div class="collapse menu-dropdown" id="sidebarDashboards">
                                 <ul class="nav nav-sm flex-column">
                                     <li class="nav-item">
-                                        <a href="dashboard-analytics.html" class="nav-link" data-key="t-analytics"> Analytics </a>
+                                        <a href="dashboard-analytics.html" class="nav-link" data-key="t-analytics"> Categories </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a href="dashboard-crm.html" class="nav-link" data-key="t-crm"> CRM </a>
+                                        <a href="dashboard-crm.html" class="nav-link" data-key="t-crm"> Brands </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a href="index.html" class="nav-link" data-key="t-ecommerce"> Ecommerce </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="dashboard-crypto.html" class="nav-link" data-key="t-crypto"> Crypto </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="dashboard-projects.html" class="nav-link" data-key="t-projects"> Projects </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="dashboard-nft.html" class="nav-link" data-key="t-nft"> NFT</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li> <!-- end Dashboard Menu -->
-                        
-
-                        <li class="nav-item">
-                            <a class="nav-link menu-link" href="widgets.html">
-                                <i class="mdi mdi-puzzle-outline"></i> <span data-key="t-widgets">Widgets</span>
-                            </a>
-                        </li> 
-
-                        <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarMaps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarMaps">
-                                <i class="mdi mdi-map-marker-outline"></i> <span data-key="t-maps">Catálogos</span>
-                            </a>
-                            <div class="collapse menu-dropdown" id="sidebarMaps">
-                                <ul class="nav nav-sm flex-column">
-                                    <li class="nav-item">
-                                        <a href="maps-google.html" class="nav-link" data-key="t-google">
-                                            Categorías
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="maps-vector.html" class="nav-link" data-key="t-vector">
-                                            Marcas
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="maps-leaflet.html" class="nav-link" data-key="t-leaflet">
-                                            Etiquetas
-                                        </a>
+                                        <a href="index.html" class="nav-link" data-key="t-ecommerce"> Tags </a>
                                     </li>
                                 </ul>
                             </div>
                         </li>
- 
 
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="widgets.html">
+                                <i class="ri-coupon-line"></i> <span data-key="t-widgets">Cupons</span>
+                            </a>
+                        </li> 
+
+                        <li class="nav-item">
+                            <a class="nav-link menu-link" href="widgets.html">
+                                <i class="ri-shopping-bag-3-line"></i> <span data-key="t-widgets">Orders</span>
+                            </a>
+                        </li> 
                     </ul>
                 </div>
                 <!-- Sidebar -->
